7b2f46f578a6a63a8b26b373d95d5ad47f711a81
Agastya-Asthana <agastya.asthana@gmail.com>
2020-01-02 09:13:45 -0800

    Adde Vision code and service file for use with systemd

--- src/1259Vision.py
+++ src/1259Vision.py
@@ -5,7 +5,7 @@ import cv2
 import time
 import numpy as np 
 
-time.sleep(15)
+#time.sleep(15)
 
 cs = cscore.CameraServer.getInstance()
 cs.enableLogging()
@@ -25,15 +25,46 @@ Front.setResolution(640, 480)
 
 server = cs.addSwitchedCamera("SwitchedCamera")
 
-print("Assigning Variables")
-FrontCameraSetup = True
-BackCameraSetup = False
+print("Starting code stuff")
+
+#Camera width and height will essentially equal the resolution that has been hardcoded
+camWid = 640
+camHgt = 480
+
+#Variables that will be needed to do distance calculations - FIRST Gen. Variables 
+DefaultImageHeight = 480
+DefaultBallHeightPixel = 100 #WE NEED TO CALCULATE THIS
+DefaultPixelsPerInch = 50 #WE NEED TO CALCULATE THIS 
+CalibrationDistanceInch = 24 
+
+MaxPossibleAngle = 60 # MEASURED IN DEGREES 
+MaxPossibleDistance = 120 # MEASURED IN INCHES
+
+#Variables needed to distance calculations - SECOND Gen. Variables
+MaxPossibleRadius = (DefaultImageHeight / 2) #MEASURED IN INCHES 
+MinPossibleRadius = 0   #WE NEED TO CALCULATE THIS
+
+#Maximum and minimum possible HSV values to detect the ball
+minHSVBall = np.array([18, 100, 100])
+maxHSVBall = np.array([40, 255, 255])
+
+#X and Y coordinate of the center of the image
+imageCenterX = camWid / 2
+imageCenterY = camHgt / 2
+
+#Scaling the image
+img = np.zeros(shape=(640, 480, 3), dtype=np.uint8)
+scale_percent = 50 # percent of original size
+width = int(img.shape[1] * scale_percent / 100)
+height = int(img.shape[0] * scale_percent / 100)
+dim = (width, height)
+
+runCalculation = True
 
 
 def Vision():
 
-    global FrontCameraSetup
-    global BackCameraSetup
+    global img
 
     try:
     	cameraFeed = SmartDashboard.getNumber("cameraFeed", 0)
@@ -49,24 +80,56 @@ def Vision():
             SmartDashboard.putString("VisionCodeSelected", "0")
         except:
             print("Cannot put string because network table was not found")
-        #server.setSource(Front)
+       
         Front.setConnectionStrategy(cscore.VideoSource.ConnectionStrategy.kKeepOpen)
         Back.setConnectionStrategy(cscore.VideoSource.ConnectionStrategy.kForceClose)
-
-        #mjpegServer = cscore.MjpegServer("httpserver", 8081)
-        #mjpegServer.setSource(Front)
 
         cvSink = cscore.CvSink("FrontSink")
         cvSink.setSource(Front)
 
         cvSource = cscore.CvSource("FrontCVSource", cscore.VideoMode.PixelFormat.kMJPEG, 640, 480, 30)
-        #cvMjpegServer = cscore.MjpegServer("FrontHttpServer", 8082)
-        #cvMjpegServer.setSource(cvSource)
-            
-        img = np.zeros(shape=(640, 480, 3), dtype=np.uint8)
                 
         time, img = cvSink.grabFrame(img)
-        cvSource.putFrame(img)
+
+        img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+
+        #Convert the RGB image to HSV
+        imHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        #Find pixels in the blurred image that fit in range and turn them white and others black                                          
+        InRange = cv2.inRange(imHSV, minHSVBall, maxHSVBall)
+
+        InRange = cv2.GaussianBlur(InRange, (5, 5), cv2.BORDER_DEFAULT)
+
+        circles = cv2.HoughCircles(InRange, cv2.HOUGH_GRADIENT, 1, int(height/10), 100, 25, 5, int(height/2))
+        circles = np.uint16(np.around(circles))
+
+        biggest_radius = 0;
+        for i in circles[0,:]:
+            try:
+
+                if (biggest_radius < i[2]):
+                    biggest_radius = i[2]
+                    biggestX = i[0]
+                    biggestY = i[1]
+            except IndexError:
+                print("No ball found")
+                runCalculation = False
+                continue
+        
+        if runCalculation:
+            
+            ActualBallHeightPixel = DefaultBallHeightPixel / (DefaultImageHeight / height)
+            ActualPixelsPerInch = DefaultPixelsPerInch / (DefaultImageHeight / height)
+
+            DirectDistanceBallInch = ((ActualBallHeightPixel / (2 * biggest_radius)) * CalibrationDistanceInch)
+            XDisaplacementPixel = biggestX - (width / 2)
+            YDisplacmentPixel = biggestY - (height / 2)
+
+            draw = cv2.circle(draw, (biggestX, biggestY), biggest_radius, (255, 0, 0), 5)
+
+        else:
+            print("could not find any object so decided to skip calculations as well")
+
 
     if cameraFeed == 1:
 
